7ffd80c76e59eb62db8504fc004e8ed6a827abbc
Evan Wilcox <ewilcox811@gmail.com>
2022-04-12 11:05:47 -0700

    Changed to an entirely new library, hikari and hikari-lightbulb.  Ping command no longer responds with the ping of the user still figuring out how to retrieve that statistic with the new library.

--- DiscordBotTut.py
+++ DiscordBotTut.py
@@ -1,57 +1,56 @@
-import os
-import discord
+import lightbulb
+import hikari
 import random
 import time
 import asyncio
 
-from dotenv import load_dotenv
-from discord.ext import commands
 
-# server id = 756657210087571466
-messages = 0
-joined = 0
+bot = lightbulb.BotApp(
+    token='NzU2NjUzNjQ0MDkwNDQxNzQ5.X2U-ng.K-SuDt3PsIpJn7k7AQ70TPrvPZc',
+    default_enabled_guilds=(963463443220947026))
 
-load_dotenv()
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print(event.content)
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
-bot = commands.Bot(command_prefix = '.')
+@bot.listen(hikari.StartedEvent)
+async def bot_started(event):
+    print("Bot has started!")
 
-print(discord.__version__)
 
-## This is a background task that updates the Stats.txt file automatically every 60 seconds.
-## It adds the time, number of messages and the number of members joined within that 60 second
-## timeframe.  If no one sends a message or joins in that timeframe it will print 0 to the file.
-# async def update_stats():
-#     await client.wait_until_ready()
-#     global messages, joined
-#
-#     while not client.is_closed():
-#         try:
-#             with open("stats.txt", "a") as f:
-#                 f.write(f"Time:  {int(time.time())}, Messagess: {messages}, Members Joines: {joined}\n")
-#             messages = 0
-#             joined = 0
-#
-#             await asyncio.sleep(60)
-#         except Exception as e:
-#             print(e)
-#             await asyncio.sleep(60)
-
-@bot.event
-async def on_ready():
-    print(f'{bot.user} has connected to Discord!\n{discord.__version__}')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Hello {ctx.author}')
-
-@bot.command()
+# Ping Command that responds with pong and the user's Ping
+# The order of these decorators is important
+@bot.command
+@lightbulb.command('ping', 'Says pong!')
+@lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx):
-    await ctx.send(f'Pong! {round(bot.latency * 1000)}ms')
+    await ctx.respond(f'Pong!')
 
-@bot.command(aliases = ['8ball', 'test'])
-async def _8ball(ctx, *, question):
+@bot.command
+@lightbulb.command('group', 'This is a group')
+@lightbulb.implements(lightbulb.SlashCommandGroup)
+async def my_group(ctx):
+    pass
+
+@my_group.child
+@lightbulb.command('subcommand', 'This is a subcommand')
+@lightbulb.implements(lightbulb.SlashSubCommand)
+async def subcommand(ctx):
+    await ctx.respond('I am a subcommand.')
+
+@bot.command
+@lightbulb.option('num2', 'The first number', type=int)
+@lightbulb.option('num1', 'The second number', type=int)
+@lightbulb.command('add', 'Add two numbers together')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def add(ctx):
+    await ctx.respond(ctx.options.num1 + ctx.options.num2)
+
+@bot.command
+@lightbulb.option('question', 'Question asked')
+@lightbulb.command('eightball', 'Shake the magic 8 ball.')
+@lightbulb.implements(lightbulb.SlashCommand)
+async def eightBall(ctx):
     responses = ['As i see it, yes.',
                  'Ask again later.',
                  'Better not tell you now.',
@@ -72,20 +71,5 @@ async def _8ball(ctx, *, question):
                  'Yes.',
                  'Yes - definitely.',
                  'You may rely on it.']
-
-    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
-
-@bot.command()
-async def clear(ctx, amount = 5):
-    await ctx.channel.purge(limit = amount)
-
-# @client.command()
-# async def kick(ctx, member : discord.Member, *, reason = None):
-#     await member.kick(reason = reason)
-#
-# @client.command()
-# async def ban(ctx, member : discord.Member, *, reason = None):
-#     await member.ban(reason = reason)
-
-
-client.run(TOKEN)
+    await ctx.respond(ctx.options.question + f'\nAnswer:\t\t{random.choice(responses)}')
+bot.run()
